94c05224761df7989ae5cfcdf628f42cfba4e34a
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2025-05-09 18:26:32 +0530

    b

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: ['https://codeforge-dyvj.onrender.com/'],
+  origin: ['https://codeforge-dyvj.onrender.com'],
   credentials: true
 }));
 app.use(session({
