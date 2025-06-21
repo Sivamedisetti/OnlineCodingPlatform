6236abf30d0a376c07de7853aeea8b59399120f4
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2025-06-21 12:27:01 +0530

    add google and github auth buttons

--- src/components/loginForm/signup.jsx
+++ src/components/loginForm/signup.jsx
@@ -88,10 +88,10 @@ function Signup({ onLogin }) {
             required
           />
           <div className="social-button" style={{marginTop: '0.5rem'}}>
-            <button className="google" onClick={() => googleLogin(onLogin)}>
+            <button type="button" className="google" onClick={() => googleLogin(onLogin)}>
               <img src={google} alt="google" className="social-img"/> &nbsp; Google
             </button>
-            <button className="github" onClick={() => githubLogin(onLogin)}>
+            <button type="button" className="github" onClick={() => githubLogin(onLogin)}>
               <img src={github} alt="github" className="social-img"/> &nbsp; Github
             </button>
           </div>
